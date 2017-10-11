e6277f94585a4342e0323fac210db5c1bd974a33
Erik Søe Sørensen <ess@trifork.com>
2017-10-11 21:12:29 +0200

    Elm: Mem-edit validation feedback; IP pointer visualization.

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -5,10 +5,11 @@ import Html.Events exposing (..)
 import Html.App
 import Array
 import String
+import Char
 
 --========== Machine environment ====================--
 type MachineSpeed = Paused | RunningSlow | RunningFast
-type alias MemoryEditState = { addr : Int, value : String }
+type alias MemoryEditState = { addr : Int, value : String, validated : Maybe Int }
 
 type alias State = {
   machineState: Model.MachineState,
@@ -39,9 +40,9 @@ update msg model =
   (case msg of
     MemoryEvent(SelectMem addr) ->
       let memValue = Maybe.withDefault 0 (Array.get addr model.machineState.memory)
-      in {model | editingMemory=Just {addr=addr, value=(toString memValue)}}
+      in {model | editingMemory=Just {addr=addr, value=(toString memValue), validated=Just memValue}}
     MemoryEvent(UpdateMemEdit addr value) ->
-      {model | editingMemory=Just {addr=addr, value=value}}
+      {model | editingMemory=Just {addr=addr, value=value, validated=validateDatum value}}
     MemoryEvent(UpdateMem addr valueStr) ->
       let ms = model.machineState
       in case String.toInt valueStr of
@@ -49,6 +50,12 @@ update msg model =
         Err reason -> model --TODO: Error feedback
     other -> model
    , Cmd.none)
+
+validateDatum : String -> Maybe Int
+validateDatum s =
+  case String.toInt s of
+    Ok value -> Just value
+    Err reason -> Nothing
 
 subscriptions : State -> Sub Msg
 subscriptions model = Sub.none
@@ -59,41 +66,53 @@ view model = body [
      ] [
      div [style [ ("color", "white") ]] [
      text "Hej!",
-     memoryView model.machineState.memory model.editingMemory
+     memoryView model.machineState.memory {editingState=model.editingMemory, ip=model.machineState.ip}
      ]
      ]
 
-memoryView mem editingMemory =
-           div [] [
-             table [] [
-               thead [] [text "Memory"],
-                tbody [] (memoryViewRows mem editingMemory)]
-           ]
+memoryView mem state =
+  div [] [
+    table [] [
+      thead [] [text "Memory"],
+       tbody [] (memoryViewRows mem state)]
+  ]
 
-memoryViewRows mem editingMemory =
-  Array.toList (Array.indexedMap (memoryViewRow editingMemory) mem)
+memoryViewRows mem state =
+  Array.toList (Array.indexedMap (memoryViewRow state) mem)
 
-memoryViewRow editingMemory memAddr memValue =
+ipArrow =
+  String.fromChar (Char.fromCode 0x25B6) -- "Black Right-Pointing Triangle"
+
+memoryViewRow state memAddr memValue =
+  tr [] [
+    td [] (if memAddr == state.ip
+           then [ span [style [("color", "#6f6")]] [text ipArrow] ]
+           else []),
+    td [style [("text-align","right")] ] [text ((toString memAddr)++":")],
+    td [] (memoryValueEditCell memAddr memValue state)
+  ]
+
+memoryValueEditCell memAddr memValue state =
   let editingValue =
-    case editingMemory of
-     Just({addr,value}) -> if addr==memAddr then Just value else Nothing
-     Nothing -> Nothing
-  in tr [] [
-      td [style [("text-align","right")] ] [text ((toString memAddr)++":")],
-      case editingValue of
-        Nothing ->
-          td [onClick (MemoryEvent(UpdateMemEdit memAddr (toString memValue)))] [text (toString memValue)]
-        Just curValue ->
-          td [] [
-            Html.input [
-              placeholder curValue,
-              --focused True,
-              value curValue,
-              onInput (\v->MemoryEvent(UpdateMemEdit memAddr v)),
-              onBlur (MemoryEvent(UpdateMem memAddr curValue))
-            ] []
-          ]
-    ]
+        case state.editingState of
+         Just em -> if em.addr==memAddr then Just em else Nothing
+         Nothing -> Nothing
+      (curValue,bgcolor) =
+        case editingValue of
+          Nothing -> (toString memValue, "#eef")
+          Just {value,validated} -> (value, case validated of
+                                              Nothing -> "#fcc"
+                                              Just _ -> "#fff")
+  in [
+      Html.input [
+        style [("text-align", "right"), ("backgroundColor", bgcolor)],
+        value curValue,
+        onFocus (MemoryEvent(UpdateMemEdit memAddr (toString memValue))),
+        onClick (MemoryEvent(UpdateMemEdit memAddr (toString memValue))),
+        onInput (\v->MemoryEvent(UpdateMemEdit memAddr v)),
+        onBlur (MemoryEvent(UpdateMem memAddr curValue))
+      ] []
+     ]
 
 main : Program Never
 main =
@@ -104,7 +123,7 @@ main =
     , subscriptions = subscriptions
     }
 
--- Function `program` is expecting the argument to be:
+-- signature of `program` :
 --     { init : ( a, Cmd b )
 --     , subscriptions : a -> Sub b
 --     , update : b -> a -> ( a, Cmd b )

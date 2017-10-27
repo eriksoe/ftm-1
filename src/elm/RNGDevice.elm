51c041c11328d63ae4c6b9e228b97fd1bb4d282d
Erik Søe Sørensen <ess@trifork.com>
2017-10-27 16:54:47 +0200

    Elm: Implement RNGDevice core functionality.

--- src/elm/RNGDevice.elm
+++ src/elm/RNGDevice.elm
@@ -35,18 +35,31 @@ setSeed : Int -> State -> State
 setSeed rnd model =
   {model | seed=Just(Random.initialSeed rnd)}
 
-input : (State, Address) -> Datum
-input (model,addr) =
-  0 --TODO
+input : (State, Address) -> (State,Datum)
+input (state,addr) =
+  let min = minValue(state)
+      max = maxValue(state)
+  in case state.seed of
+     Nothing ->
+       let _ = Debug.log "Rnd error: seed not set :-(" state.seed
+       in (state, min)
+     Just seed ->
+       if min < max
+       then
+         let (rnd,newSeed) = Random.step (Random.int min max) seed
+         in ({state | seed=Just newSeed}, rnd)
+       else
+         (state, min)
 
 output : (State, Address, Datum) -> State
 output (state, addr, value) =
-  state --TODO
+  if addr>=1 && addr<=2
+  then {state | control = Array.set (addr-1) value state.control}
+  else state
 
 render : (State) -> Html IODeviceCmd
 render model =
   Html.div [] [
-  Html.text "<RNG device>",
   Html.br [] [], Html.text "Min: ", Html.text (toString (minValue model)),
   Html.br [] [], Html.text "Max: ", Html.text (toString (maxValue model)),
   Html.br [] [], Html.text "Seed: ", Html.text (toString model.seed)
